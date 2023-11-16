52ae66e0dd944a8f186f442a762dc3231f331482
Nivea Vilar <niveavilar@gmail.com>
2023-11-16 17:43:17 +0100

    created almost all methods for bookmarks, just need to add the destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "cleaning database..."
+Movie.destroy_all
+
+puts "Creating movies..."
+wonder_woman = { title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6 }
+the_shawshack = { title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8 }
+titanic = { title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7 }
+oceans_eight = { title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7 }
+
+[wonder_woman, the_shawshack, titanic, oceans_eight].each do |attributes|
+  movie = Movie.create!(attributes)
+  puts "Created #{movie.title}"
+end
+puts "Finished!"

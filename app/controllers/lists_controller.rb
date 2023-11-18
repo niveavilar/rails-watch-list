980bd2b3c3c9ff53b251ed8618eec1d2f8c4abd1
Nivea Vilar <niveavilar@gmail.com>
2023-11-18 15:27:14 +0100

    banner and button

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,8 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
+    options = { width: 1000, height: 600, crop: :fill }
+    @banner_url = Cloudinary::Utils.cloudinary_url('cinema_y074eh', options)
   end
 
   def show
@@ -20,9 +22,19 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to list_path(@list)
+  end
+
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :lists, only: %i[index show new create] do
+  resources :lists, only: %i[index show new create edit update] do
     resources :bookmarks, only: %i[new create destroy]
   end
   # Defines the root path route ("/")
